c0e2247b5c858ff3a986a557c1100fb7aa0f261d
shelbipittman <122328488+shelbipittman@users.noreply.github.com>
2023-04-24 18:21:20 -0400

    Update hangman.py
    Comments removed to read code better

--- hangman.py
+++ hangman.py
@@ -1,15 +1,3 @@
-"""
-Hangman implementation by Kylie Ying
-
-YouTube Kylie Ying: https://www.youtube.com/ycubed 
-Twitch KylieYing: https://www.twitch.tv/kylieying 
-Twitter @kylieyying: https://twitter.com/kylieyying 
-Instagram @kylieyying: https://www.instagram.com/kylieyying/ 
-Website: https://www.kylieying.com
-Github: https://www.github.com/kying18 
-Programmer Beast Mode Spotify playlist: https://open.spotify.com/playlist/4Akns5EUb3gzmlXIdsJkPs?si=qGc4ubKRRYmPHAJAIrCxVQ 
-"""
-
 import random
 from words import words
 from hangman_visual import lives_visual_dict
@@ -17,7 +5,7 @@ import string
 
 
 def get_valid_word(words):
-    word = random.choice(words)  # randomly chooses something from the list
+    word = random.choice(words)  
     while '-' in word or ' ' in word:
         word = random.choice(words)
 
@@ -26,19 +14,16 @@ def get_valid_word(words):
 
 def hangman():
     word = get_valid_word(words)
-    word_letters = set(word)  # letters in the word
+    word_letters = set(word) 
     alphabet = set(string.ascii_uppercase)
-    used_letters = set()  # what the user has guessed
+    used_letters = set()  
 
     lives = 7
 
-    # getting user input
     while len(word_letters) > 0 and lives > 0:
-        # letters used
-        # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
+
         print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
-        # what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
         print(lives_visual_dict[lives])
         print('Current word: ', ' '.join(word_list))
@@ -51,7 +36,7 @@ def hangman():
                 print('')
 
             else:
-                lives = lives - 1  # takes away a life if wrong
+                lives = lives - 1  
                 print('\nYour letter,', user_letter, 'is not in the word.')
 
         elif user_letter in used_letters:
@@ -60,7 +45,6 @@ def hangman():
         else:
             print('\nThat is not a valid letter.')
 
-    # gets here when len(word_letters) == 0 OR when lives == 0
     if lives == 0:
         print(lives_visual_dict[lives])
         print('You died, sorry. The word was', word)
